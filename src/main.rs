b8b8d7a32b8c4223e44dc4a901b78d6097234b10
eturzinski <Erik.Turzinski@adesso.de>
2019-03-11 23:07:50 +0100

    changed some shit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-use rpi_led_matrix::{LedMatrixOptions, LedMatrix, LedCanvas, LedColor};
+use rpi_led_matrix::{LedCanvas, LedColor, LedMatrix, LedMatrixOptions};
 
 fn main() {
     let mut options = LedMatrixOptions::new();
     options.set_rows(16);
     options.set_hardware_mapping("adafruit-hat");
-    let matrix = LedMatrix::new(Some(options)).unwrap();
-    let mut canvas:LedCanvas = matrix.canvas();
-    canvas.fill(&LedColor{red:10,green:0,blue:0})
+    let matrix;
+    matrix = match LedMatrix::new(Option::from(options)) {
+        Ok(x) => x,
+        Err(x) => panic!(x)
+    };
+    let mut canvas: LedCanvas = matrix.canvas();
+    canvas.fill(&LedColor { red: 10, green: 0, blue: 0 });
+//    let stdin = std::io::stdin();
+//    let mut string = String::new();
+//    loop {
+//        stdin.read_line(&mut string);
+//        if string == "exit" { break; }
+//    }
 }
