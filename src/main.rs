1d5c0dc7af823d51f2b40b47bf97301c8e0dc602
jturzinski <janos.turzinski@adesso.de>
2019-03-14 22:40:17 +0100

    first try print hello world

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,17 @@ fn main() {
     let mut options = setup_options(100);
     let mut matrix = LedMatrix::new(Some(options)).unwrap();
     let mut canvas: LedCanvas = matrix.canvas();
+    let text = "HELLO WORLD";
 
     let map = setup_letters();
-    printLetter('A',&mut canvas,&map);
 
-    std::thread::sleep(std::time::Duration::new(5,0));
-
-    printLetter('B',&mut canvas,&map);
-    loop {}
-}
+    loop {
+        for l in text.chars() {
+            printLetter(l,&mut canvas,&map);
+            std::thread::sleep(std::time::Duration::new(1,0));
+        }
+    }
+    }
 
 fn setup_options(brightness:u8)->LedMatrixOptions{
     let mut options = LedMatrixOptions::new();
@@ -31,6 +33,33 @@ fn setup_letters() -> HashMap<char,[[bool; 6]; 5] > {
     let mut map = HashMap::new();
     map.insert ( 'A',[[true,true,true,true,true,true],[true,false,true,false,false,false],[true,false,true,false,false,false],[true,false,true,false,false,false],[true,true,true,true,true,true]]);
     map.insert ( 'B',[[true,true,true,true,true,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[false,true,true,true,true,false]]);
+    map.insert ( 'C',[[true,true,true,true,true,true],[true,false,false,false,false,true],[true,false,false,false,false,true],[true,false,false,false,false,true],[true,false,false,false,false,true]]);
+    map.insert ( 'D',[[true,true,true,true,true,true],[true,false,false,false,false,true],[true,false,false,false,false,true],[true,false,false,false,false,true],[false,true,true,true,true,false]]);
+    map.insert ( 'E',[[true,true,true,true,true,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[true,false,true,false,false,true]]);
+    map.insert ( 'F',[[true,true,true,true,true,true],[true,false,true,false,false,false],[true,false,true,false,false,false],[true,false,true,false,false,false],[true,false,true,false,false,false]]);
+    map.insert ( 'G',[[true,true,true,true,true,true],[true,false,false,false,false,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[true,false,false,true,true,true]]);
+    map.insert ( 'H',[[true,true,true,true,true,true],[false,false,true,false,false,false],[false,false,true,false,false,false],[false,false,true,false,false,false],[true,true,true,true,true,true]]);
+    map.insert ( 'I',[[true,true,true,true,true,true],[false,false,false,false,false,false],[false,false,false,false,false,false],[false,false,false,false,false,false],[false,false,false,false,false,false]]);
+    map.insert ( 'J',[[true,false,false,true,true,false],[true,false,false,false,false,true],[true,false,false,false,false,true],[true,false,false,false,false,true],[true,true,true,true,true,true]]);
+    map.insert ( 'K',[[true,true,true,true,true,true],[false,false,true,false,false,false],[false,true,false,true,false,false],[true,false,false,false,true,false],[false,false,false,false,false,true]]);
+    map.insert ( 'L',[[true,true,true,true,true,true],[false,false,false,false,false,true],[false,false,false,false,false,true],[false,false,false,false,false,true],[false,false,false,false,false,true]]);
+    map.insert ( 'M',[[true,true,true,true,true,true],[true,false,false,false,false,false],[false,true,true,false,false,false],[true,false,false,false,false,false],[true,true,true,true,true,true]]);
+    map.insert ( 'N',[[true,true,true,true,true,true],[false,true,false,false,false,false],[false,false,true,false,false,false],[false,false,false,true,false,false],[true,true,true,true,true,true]]);
+    map.insert ( 'O',[[false,true,true,true,true,false],[true,false,false,false,false,true],[true,false,false,false,false,true],[true,false,false,false,false,true],[false,true,true,true,true,false]]);
+    map.insert ( 'P',[[true,true,true,true,true,true],[true,false,false,true,false,false],[true,false,false,true,false,false],[true,false,false,true,false,false],[false,true,true,false,false,false]]);
+    map.insert ( 'Q',[[false,true,true,true,false,false],[true,false,false,false,true,false],[true,false,false,true,true,false],[true,false,false,false,true,false],[false,true,true,true,false,true]]);
+    map.insert ( 'R',[[true,true,true,true,true,true],[true,false,false,true,false,false],[true,false,false,true,false,false],[true,false,false,true,true,false],[false,true,true,false,false,true]]);
+    map.insert ( 'S',[[false,true,false,false,false,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[true,false,false,true,true,false]]);
+    map.insert ( 'T',[[true,false,false,false,false,false],[true,false,false,false,false,false],[true,true,true,true,true,true],[true,false,false,false,false,false],[true,false,false,false,false,false]]);
+    map.insert ( 'U',[[true,true,true,true,true,false],[false,false,false,false,false,true],[false,false,false,false,false,true],[false,false,false,false,false,true],[true,true,true,true,true,false]]);
+    map.insert ( 'V',[[true,true,true,true,false,false],[false,false,false,false,true,false],[false,false,false,false,false,true],[false,false,false,false,true,false],[true,true,true,true,false,false]]);
+    map.insert ( 'W',[[true,true,true,true,true,true],[false,false,false,false,true,false],[false,false,false,false,false,true],[false,false,false,false,true,false],[true,true,true,true,true,true]]);
+    map.insert ( 'X',[[true,false,false,false,false,true],[false,true,false,false,true,false],[false,false,true,true,false,false],[false,true,false,false,true,false],[true,false,false,false,false,true]]);
+    map.insert ( 'Y',[[true,false,false,false,false,false],[false,true,false,false,false,false],[false,false,true,true,true,true],[false,true,false,false,false,false],[true,false,false,false,false,false]]);
+    map.insert ( 'Z',[[true,false,false,false,true,true],[true,false,false,true,false,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[true,true,false,false,false,true]]);
+    map.insert ( ' ',[[false,false,false,false,false,false],[false,false,false,false,false,false],[false,false,false,false,false,false],[false,false,false,false,false,false],[false,false,false,false,false,false]]);
+    map.insert ( '!',[[false,false,false,false,false,false],[false,false,false,false,false,false],[true,true,true,true,false,true],[false,false,false,false,false,false],[false,false,false,false,false,false]]);
+
     map
 }
 
