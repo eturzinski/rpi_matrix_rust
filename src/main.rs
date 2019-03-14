e7b08b39c8598149d4ce4535709bf563ea1c3973
eturzinski <Erik.Turzinski@adesso.de>
2019-03-14 19:23:56 +0100

    test brightness

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,27 @@ extern crate rpi_led_matrix;
 use rpi_led_matrix::{LedCanvas, LedColor, LedMatrix, LedMatrixOptions};
 
 fn main() {
+    let mut options = setup_options(100);
+    let mut matrix = LedMatrix::new(Some(options)).unwrap();
+    let mut canvas: LedCanvas = matrix.canvas();
+    canvas.fill(&LedColor { red: 10, green: 0, blue: 0 });
+
+    std::thread::sleep(std::time::Duration::new(10,0));
+
+    let mut options = setup_options(10);
+    let mut matrix = LedMatrix::new(Some(options)).unwrap();
+    let mut canvas: LedCanvas = matrix.canvas();
+    canvas.fill(&LedColor { red: 10, green: 0, blue: 0 });
+
+    loop {}
+}
+
+fn setup_options(brightness:u8)->LedMatrixOptions{
     let mut options = LedMatrixOptions::new();
     options.set_rows(16);
     options.set_hardware_mapping("adafruit-hat");
     options.set_chain_length(1);
     options.set_parallel(true);
-    options.set_brightness(50);
-    let matrix = LedMatrix::new(Some(options)).unwrap();
-    let mut canvas: LedCanvas = matrix.canvas();
-    canvas.fill(&LedColor { red: 10, green: 0, blue: 0 });
-    loop {}
+    options.set_brightness(brightness);
+    options
 }
