11332e41e7fb89eee7ae83a0d7a1aa63b1bf9c4b
jturzinski <janos.turzinski@adesso.de>
2019-03-14 22:13:31 +0100

    first try print letters A&B

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 extern crate rpi_led_matrix;
 
 use rpi_led_matrix::{LedCanvas, LedColor, LedMatrix, LedMatrixOptions};
+use std::collections::HashMap;
 
 fn main() {
     let mut options = setup_options(100);
     let mut matrix = LedMatrix::new(Some(options)).unwrap();
     let mut canvas: LedCanvas = matrix.canvas();
-    canvas.fill(&LedColor { red: 10, green: 0, blue: 0 });
 
-    std::thread::sleep(std::time::Duration::new(10,0));
+    let map = setup_letters();
+    printLetter(A,&mut canvas,&map);
 
-    options = setup_options(10);
-    matrix = LedMatrix::new(Some(options)).unwrap();
-    canvas = matrix.canvas();
-    canvas.fill(&LedColor { red: 10, green: 0, blue: 0 });
+    std::thread::sleep(std::time::Duration(5,0));
 
+    printLetter(B,&mut canvas,&map);
     loop {}
 }
 
@@ -26,4 +25,21 @@ fn setup_options(brightness:u8)->LedMatrixOptions{
     options.set_parallel(true);
     options.set_brightness(brightness);
     options
+}
+
+fn setup_letters() -> HashMap<char,[[bool; 6]; 5] > {
+    let mut map = HashMap::new();
+    map.insert ( 'A',[[true,true,true,true,true,true],[true,false,true,false,false,false],[true,false,true,false,false,false],[true,false,true,false,false,false],[true,true,true,true,true,true]]);
+    map.insert ( 'B',[[true,true,true,true,true,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[true,false,true,false,false,true],[false,true,true,true,true,false]]);
+    map
+}
+
+fn printLetter(key: char, can: &mut LedCanvas,map: &HashMap<char, [[bool; 6]; 5]>){
+    can.clear();
+    let letter = map.get(&key)?;
+    for i in 0..letter.len() {
+        for j in 0..letter[i].len(){
+            can.set(i as i32,j as i32,&LedColor { red: 10, green: 0, blue: 0 });
+        }
+    }
 }
