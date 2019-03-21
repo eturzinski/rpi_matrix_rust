eb0d1bf4e387c992de6818cdd7a68da283892e2f
eturzinski <Erik.Turzinski@adesso.de>
2019-03-21 22:12:53 +0100

    fix

---
+++ src/letters.rs
@@ -0,0 +1,18 @@
+
+
+use std::fs::File;
+use std::io::Read;
+use std::collections::HashMap;
+use std::path::Path;
+
+#[derive(Serialize, Deserialize)]
+pub struct Font{
+    pub letters:HashMap<char, [[bool; 6]; 5]>,
+}
+impl Font{
+    pub fn from_file<P: AsRef<Path>>(path: P)->Font{
+        let file = File::open(path).unwrap();
+        serde_json::from_reader(file).unwrap()
+    }
+
+}
