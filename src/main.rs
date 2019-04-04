31b850c8bfbac26bb5b365680dea51d52e749748
eturzinski <Erik.Turzinski@adesso.de>
2019-04-04 23:12:37 +0200

    Revert "refactor part 1"
    This reverts commit 4879f6a6

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,7 @@ extern crate serde_derive;
 extern crate serde_json;
 
 use std::collections::{HashMap, VecDeque};
-use std::sync::atomic::AtomicBool;
-use std::sync::mpsc::{channel, Sender};
 use std::sync::Mutex;
-use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -24,34 +21,31 @@ use rocket::request::Form;
 use rpi_led_matrix::{LedCanvas, LedColor, LedMatrix, LedMatrixOptions};
 
 use font::Font;
+use std::sync::atomic::AtomicBool;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
 
 mod font;
-mod print;
 
 struct PrintData {
-    text: String,
-    speed: usize,
+    text:String,
+    speed:usize
 }
 
-fn main() {
-    let (tx, rx) = channel::<PrintData>();
 
-    thread::spawn(move || {
-        let mut options = setup_options(100);
-        let mut matrix = LedMatrix::new(Some(options)).unwrap();
-        let mut canvas: LedCanvas = matrix.canvas();
-        let map = Font::from_file("font.json").letters;
-        for received in rx {
-            print::print_text_ticker(received.text.as_str(), &mut canvas, &map, received.speed);
-        }
-    });
+#[options("/<txt>/<sleep>")]
+fn index(txt: String, sleep: usize,sender: State<Mutex<Sender<PrintData>>>) -> &'static str {
 
-    rocket::ignite()
-        .manage(Mutex::new(tx.clone()))
-        .register(catchers![not_found])
-        .mount("/", routes![index])
-        .launch();
+    sender.lock().unwrap().send(PrintData{text:txt,speed:sleep});
+    "hi"
 }
+
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Du dumme Sau, so geht das nicht!")
+}
+
 
 fn setup_options(brightness: u8) -> LedMatrixOptions {
     let mut options = LedMatrixOptions::new();
@@ -63,13 +57,61 @@ fn setup_options(brightness: u8) -> LedMatrixOptions {
     options
 }
 
-#[options("/<txt>/<sleep>")]
-fn index(txt: String, sleep: usize, sender: State<Mutex<Sender<PrintData>>>) -> &'static str {
-    sender.lock().unwrap().send(PrintData { text: txt, speed: sleep });
-    "hi"
+fn main() {
+
+    let(tx,rx)=channel::<PrintData>();
+
+    thread::spawn(move ||{
+        let mut options = setup_options(100);
+        let mut matrix = LedMatrix::new(Some(options)).unwrap();
+        let mut canvas: LedCanvas = matrix.canvas();
+        let map = Font::from_file("font.json").letters;
+        for received in rx {
+            print_text_ticker(received.text.as_str(),&mut canvas,&map,received.speed);
+        }
+    });
+
+    rocket::ignite()
+        .manage(Mutex::new(tx.clone()))
+        .register(catchers![not_found])
+        .mount("/", routes![index])
+        .launch();
 }
 
-#[catch(404)]
-fn not_found(req: &Request) -> String {
-    format!("Du dumme Sau, so geht das nicht!")
+
+fn _print_letter(key: char, can: &mut LedCanvas, map: &HashMap<char, [[bool; 6]; 5]>) {
+    _print_letter_offset(key, can, map, 0, 0, 0);
+}
+
+fn _print_letter_offset(key: char, can: &mut LedCanvas, map: &HashMap<char, [[bool; 6]; 5]>, offset_x: usize, offset_y: usize, running_offset: usize) {
+    let letter = map.get(&key).unwrap();
+    let mut rng = rand::thread_rng();
+    for i in 0..letter.len() {
+        for j in 0..letter[i].len() {
+            if letter[i][j] && (i + offset_x) >= running_offset {
+                can.set((i + offset_x - running_offset) as i32, (j + offset_y) as i32, &LedColor { red: rng.gen_range(1, 255), green: rng.gen_range(1, 255), blue: rng.gen_range(1, 255) });
+            }
+        }
+    }
+}
+
+fn _print_text(text: &str, can: &mut LedCanvas, map: &HashMap<char, [[bool; 6]; 5]>) {
+    let letter_size = 6;
+    can.clear();
+    for i in 0..text.len() {
+        _print_letter_offset(text[i..].chars().next().unwrap(), can, map, i * letter_size, 0, 0);
+    }
+}
+
+fn print_text_ticker(text: &str, can: &mut LedCanvas, map: &HashMap<char, [[bool; 6]; 5]>, sleep: usize) {
+    let text = text.to_uppercase();
+    let dur = sleep * 1_000_000;
+    let letter_size = 6;
+    for off in 0..(letter_size * text.len()) {
+        for i in 0..text.len() {
+            _print_letter_offset(text[i..].chars().next().unwrap(), can, map, (i * letter_size), 0, off);
+        }
+        std::thread::sleep(Duration::new(0, dur as u32));
+        can.clear();
+    }
 }
